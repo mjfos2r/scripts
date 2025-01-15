398004c6c9127f7fa6bbb63f57b93048050e0a4f
Michael Foster <50351736+mjfos2r@users.noreply.github.com>
2025-01-15 16:46:29 -0500

    fixed vm script

--- vm.sh
+++ vm.sh
@@ -1,85 +1,95 @@
 #!/bin/zsh
 
 function usage() {
-	echo "Usage: $0 [-h] [-l | -1 | -0 | -c | -j] <instance>"
-	echo "This is a simple helper script to start and stop gcloud instances. "
-	echo "Options: "
-	echo "          -h                           Show this help message "
-  echo "          -l                           List instances"
-	echo "          -1                           Start instance"
-	echo "          -0                           Stop instance"
-	echo "          -c                           Connect to instance"
-	echo "          -j                           Connect to instance with port forwarding (jupyter server)"
-	echo "  <instance>                           Name of Instance"
+    echo "Usage: $0 [-h] [-l] [-1 | -0 | -c | -j] <instance>"
+    echo "This is a simple helper script to start and stop gcloud instances. "
+    echo "Options: "
+    echo "          -h                           Show this help message "
+    echo "          -l                           List instances"
+    echo "          -1                           Start instance"
+    echo "          -0                           Stop instance"
+    echo "          -c                           Connect to instance"
+    echo "          -j                           Connect to instance with port forwarding (jupyter server)"
+    echo "  <instance>                           Name of Instance"
 }
 
+# Check for no arguments
 if [[ $# -eq 0 ]]; then
-  echo "Error: Please provide an argument"
-  usage
-  exit 1
-
-elif [[ $# -lt 2 && $1 != "-h" ]]; then
-  usage
-  echo "ERROR: Please specify either -h for usage or include both an action and instance name" >&2
-  exit 2
-
-elif [[ $1 == "-h" ]]; then
-  usage
-  exit 0
-
-elif [[ $2 == "-l" || $2 == "-1"  || $2 == "-0" || $2 == "-c" || $2 == "-j" ]]; then
-  usage
-  echo "ERROR: Please only specify one action. Do not use any of: -1, -0, or -l  simultaneously." >&2
-  exit 2
-
-else
-	if [[ $1 == "-1" ]]; then
-		MODE="instances"
-		ACTION="start"
-		INSTANCE=$2
-		OPTIONS=""
-	elif [[ $1 == "-0" ]]; then
-		MODE="instances"
-		ACTION="stop"
-		INSTANCE=$2
-		OPTIONS=""
-	elif [[ $1 == "-l" ]]; then
-		MODE="instances"
-		ACTION="list"
-		INSTANCE=''
-		OPTIONS=""
-	elif [[ $1 == "-c" ]]; then
-		MODE="ssh"
-		ACTION="connect"
-		INSTANCE=$2
-		OPTIONS="--internal-ip --zone=us-central1-a"
-	elif [[ $1 == "-j" ]]; then
-		MODE="ssh"
-		ACTION="jupyter"
-		INSTANCE=$2
-		OPTIONS="--internal-ip --zone=us-central1-a -- -L 8888:localhost:8888"
-	else
-		usage
-		echo "you provided an incorrect action. please try again" >&2
-		exit 2
-	fi
+    echo "Error: Please provide an argument" >&2
+    usage
+    exit 1
 fi
 
-# source .zshrc so we can use the gcloud suite
-source ~/.zshrc
-
-if [[ $ACTION == "stop" ]]; then
-	echo "${ACTION}ping gcp instance: $INSTANCE"
-else
-	echo "${ACTION}ing gcp instance: $INSTANCE"
+# Handle single argument cases
+if [[ $# -eq 1 ]]; then
+    case $1 in
+        "-h")
+            usage
+            exit 0
+            ;;
+        "-l")
+            gcloud compute instances list
+            exit 0
+            ;;
+        *)
+            usage
+            echo "ERROR: Please specify either -h for usage, -l to list all available instances, or include both an action and instance name" >&2
+            exit 2
+            ;;
+    esac
 fi
 
-if [[ $ACTION == "connect" ]]; then
-	echo "${ACTION}ing to  gcp instance: $INSTANCE"
-elif [[ $ACTION == "jupyter" ]]; then 
-	echo "${ACTION}ing to gcp instance: $INSTANCE with port forwarding!"
+# Handle two argument cases
+if [[ $# -eq 2 ]]; then
+    # Validate argument order
+    if [[ $2 =~ ^- ]]; then
+        usage
+        echo "ERROR: Instance name should be the second argument, not an option" >&2
+        exit 2
+    fi
+    
+    INSTANCE=$2
+    ZONE="--zone=us-central1-a"
+    
+    case $1 in
+        "-1")
+            echo "Starting gcp instance: $INSTANCE"
+            exec gcloud compute instances start "$INSTANCE" $ZONE
+            ;;
+        "-0")
+            echo "Stopping gcp instance: $INSTANCE"
+            exec gcloud compute instances stop "$INSTANCE" $ZONE
+            ;;
+        "-c")
+            echo "Connecting to gcp instance: $INSTANCE"
+            exec gcloud compute ssh "$INSTANCE" $ZONE --internal-ip
+            ;;
+        "-j")
+            echo "Connecting to gcp instance: $INSTANCE with port forwarding!"
+            echo "Initializing jupyer-lab server!"
+            REMOTE_CMD='
+              echo "Current PATH: $PATH"
+              echo "Sourcing conda..."
+              source ~/bin/miniconda3/etc/profile.d/conda.sh
+              echo "Activating conda base..."
+              conda activate base
+              echo "Conda environment: $CONDA_PREFIX"
+              echo "Which jupyter-lab: $(which jupyter-lab)"
+              echo "Starting jupyter-lab..."
+              jupyter-lab --no-browser
+            '
+            exec gcloud compute ssh "$INSTANCE" $ZONE --internal-ip -- -L 8888:localhost:8888 \
+                "bash --login -c '$REMOTE_CMD'"
+            ;;
+        *)
+            usage
+            echo "ERROR: Invalid action specified. Please use -1, -0, -c, or -j" >&2
+            exit 2
+            ;;
+    esac
 fi
 
-exec gcloud compute $MODE $ACTION $INSTANCE $OPTIONS
-
-exit 0
+# If we get here, too many arguments were provided
+usage
+echo "ERROR: Too many arguments provided" >&2
+exit 2
